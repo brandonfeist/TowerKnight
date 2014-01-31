d79ce6f0647baa8afb11fb48b9edd489cd9cebb5
Brandon Feist <feist.brandon@gmail.com>
2014-01-30 18:24:25 -0800

    Moved something.

--- src/com/misterpops/towerknight/Screens/MainMenu.java
+++ src/com/misterpops/towerknight/Screens/MainMenu.java
@@ -73,7 +73,6 @@ public class MainMenu implements Screen{
 			}
 			
 			public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-				stage.clear();
 				game.setScreen(new Game(game));
 			}
 		});
@@ -119,6 +118,7 @@ public class MainMenu implements Screen{
 		skin.dispose();
 		buttonAtlas.dispose();
 		whiteFont.dispose();
+		stage.clear();
 		stage.dispose();
 	}
 
